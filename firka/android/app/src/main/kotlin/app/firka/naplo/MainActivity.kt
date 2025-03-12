04a877ca678ae196f968ddd3f9879b6ca63d6c3a
BalazsManus <contact@balazsmanus.hu>
2025-03-12 08:47:00 +0100

    disable screenshot detection thingy

---
+++ firka/android/app/src/main/kotlin/app/firka/naplo/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.firka.naplo
+
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.clearFlags(android.view.WindowManager.LayoutParams.FLAG_SECURE)
+    }
+}
