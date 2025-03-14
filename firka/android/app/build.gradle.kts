49094ab8a722840b7f2d3c7ebf637b162efb98e7
4831c0 <4831c0@proton.me>
2025-03-14 19:46:06 +0100

    implement release config

--- firka/android/app/build.gradle.kts
+++ firka/android/app/build.gradle.kts
@@ -1,8 +1,19 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+fun loadProperties(file: File): Properties {
+    val properties = Properties()
+    FileInputStream(file).use { inputStream ->
+        properties.load(inputStream)
+    }
+    return properties
 }
 
 android {
@@ -29,11 +40,46 @@ android {
         versionName = flutter.versionName
     }
 
+    val secretsDir = File(projectDir.absolutePath, "../../../secrets/")
+    val propsFile = File(secretsDir, "keystore.properties")
+
+    if (propsFile.exists()) {
+        val props = loadProperties(propsFile)
+        val store = File(secretsDir, props["storeFile"].toString())
+
+        println(
+            "Signing with:\n" +
+                    "\t- store: ${store.name}\n" +
+                    "\t- key: ${props["keyAlias"]}"
+        );
+
+        signingConfigs {
+            create("release") {
+                storeFile = store
+                storePassword = props["storePassword"] as String
+                keyPassword = props["keyPassword"] as String
+                keyAlias = props["keyAlias"] as String
+            }
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            val config = signingConfigs.findByName("release")
+
+            if (config != null) {
+                signingConfig = config
+            } else {
+                // This isn't an error, however by default flutter will hide warnings and etc.
+                // so the only way to make this show up in flutter build is to
+                // 1. make it an error
+                // 2. use println
+                // however, println doesn't bring enough attention to the warning
+                // so I decided to use logger.error
+                logger.error("[WARNING] No keystore specified! Using debug keys to sign the apk.")
+                logger.error("[WARNING] DO NOT STORE ANY SENSITIVE DATA INSIDE THE APP")
+                logger.error("[WARNING] Because an attacker could steal it, if you sideload their malicious app.")
+            }
         }
     }
 }
