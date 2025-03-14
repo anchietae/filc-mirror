2229b196b37b6c688f4d4efeaed141872b71b8aa
4831c0 <4831c0@proton.me>
2025-03-14 18:54:40 +0100

    backport: 04a877ca67
    Co-authored-by: BalazsManus <contact@balazsmanus.hu>

--- firka/android/app/src/main/kotlin/com/example/firka/MainActivity.kt
+++ firka/android/app/src/main/kotlin/com/example/firka/MainActivity.kt
@@ -1,5 +1,13 @@
 package app.firka.naplo
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.clearFlags(android.view.WindowManager.LayoutParams.FLAG_SECURE)
+    }
+
+}
