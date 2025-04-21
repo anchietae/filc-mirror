384aa74be6b9f4544f4d7fd20cac2b9ceee7de94
4831c0 <4831c0@proton.me>
2025-04-21 14:44:00 +0200

    make debug screen scrollable

--- firka/lib/ui/phone/screens/debug/debug_screen.dart
+++ firka/lib/ui/phone/screens/debug/debug_screen.dart
@@ -46,111 +46,114 @@ class _DebugScreen extends State<DebugScreen> {
         centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Debug Screen',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Debug Screen',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                Text('use cache'),
-                Switch(
-                  value: useCache,
-                  onChanged: (bool value) {
-                    setState(() {
-                      useCache = value;
-                    });
-                  },
-                )
-              ],
-            ),
-            const SizedBox(height: 20),
-            profilePicture,
-            ElevatedButton(
-              onPressed: () async {
-                await pickProfilePicture(data, _picker);
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Text('use cache'),
+                  Switch(
+                    value: useCache,
+                    onChanged: (bool value) {
+                      setState(() {
+                        useCache = value;
+                      });
+                    },
+                  )
+                ],
+              ),
+              const SizedBox(height: 20),
+              profilePicture,
+              ElevatedButton(
+                onPressed: () async {
+                  await pickProfilePicture(data, _picker);
 
-                setState(() {
-                  if (data.profilePicture != null) {
-                    profilePictureData = data.profilePicture;
-                  }
-                });
-              },
-              child: const Text('Pick pfp'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                throw 0 / 0;
-              },
-              child: const Text('Throw Exception'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                print(
-                    "getStudent(): ${await data.client.getStudent(forceCache: useCache)}");
-              },
-              child: const Text('getStudent()'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                print(
-                    "getNoticeBoard(): ${await data.client.getNoticeBoard(forceCache: useCache)}");
-              },
-              child: const Text('getNoticeBoard()'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                print(
-                    "getGrades(): ${await data.client.getGrades(forceCache: useCache)}");
-              },
-              child: const Text('getGrades()'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                var now = DateTime.now();
+                  setState(() {
+                    if (data.profilePicture != null) {
+                      profilePictureData = data.profilePicture;
+                    }
+                  });
+                },
+                child: const Text('Pick pfp'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  throw 0 / 0;
+                },
+                child: const Text('Throw Exception'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  print(
+                      "getStudent(): ${await data.client.getStudent(forceCache: useCache)}");
+                },
+                child: const Text('getStudent()'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  print(
+                      "getNoticeBoard(): ${await data.client.getNoticeBoard(forceCache: useCache)}");
+                },
+                child: const Text('getNoticeBoard()'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  print(
+                      "getGrades(): ${await data.client.getGrades(forceCache: useCache)}");
+                },
+                child: const Text('getGrades()'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  var now = DateTime.now();
 
-                var start = now.subtract(Duration(days: 14));
-                var end = now.add(Duration(days: 7));
+                  var start = now.subtract(Duration(days: 14));
+                  var end = now.add(Duration(days: 7));
 
-                print(
-                    "getLessons(): ${await data.client.getTimeTable(start, end, forceCache: useCache)}");
-              },
-              child: const Text('getLessons()'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                var now = DateTime.now();
+                  print(
+                      "getLessons(): ${await data.client.getTimeTable(start, end, forceCache: useCache)}");
+                },
+                child: const Text('getLessons()'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  var now = DateTime.now();
 
-                var start = now.subtract(Duration(days: 7));
-                var end = now.add(Duration(days: 14));
+                  var start = now.subtract(Duration(days: 7));
+                  var end = now.add(Duration(days: 14));
 
-                print(
-                    "getHomework(): ${await data.client.getHomework(start, end, forceCache: useCache)}");
-              },
-              child: const Text('getHomework()'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                print(
-                    "getTests(): ${await data.client.getTests(forceCache: useCache)}");
-              },
-              child: const Text('getTests()'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                print(
-                    "getOmissions(): ${await data.client.getOmissions(forceCache: useCache)}");
-              },
-              child: const Text('getOmissions()'),
-            ),
-          ],
+                  print(
+                      "getHomework(): ${await data.client.getHomework(start, end, forceCache: useCache)}");
+                },
+                child: const Text('getHomework()'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  print(
+                      "getTests(): ${await data.client.getTests(forceCache: useCache)}");
+                },
+                child: const Text('getTests()'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  print(
+                      "getOmissions(): ${await data.client.getOmissions(forceCache: useCache)}");
+                },
+                child: const Text('getOmissions()'),
+              ),
+              SizedBox(height: 32),
+            ],
+          ),
         ),
       ),
     );
