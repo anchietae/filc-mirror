92b3a47fdc6fa7407224ea4870d4c560f9d968eb
Armand <4831c0@proton.me>
2025-05-12 11:35:33 +0200

    Add nix devshell

---
+++ flake.nix
@@ -0,0 +1,136 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    android-nixpkgs.url = "github:tadfisher/android-nixpkgs";
+  };
+
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://cache.onyx.ovh"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cache.onyx.ovh:2wUG6wsx5slbKUgkHT6GJuQ5k2StuUc8ysZQ2W+fbxA="
+    ];
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+      perSystem = {
+        pkgs,
+        system,
+        lib,
+        ...
+      }: {
+        devShells.default = let
+          pkgs = import nixpkgs {
+            inherit system;
+            config = {
+              allowUnfree = true;
+              android_sdk.accept_license = true;
+            };
+            overlays = [];
+          };
+          android-nixpkgs = pkgs.callPackage inputs.android-nixpkgs {};
+          androidSdk = android-nixpkgs.sdk (sdkPkgs:
+            with sdkPkgs; [
+              cmdline-tools-latest
+              build-tools-34-0-0
+              build-tools-30-0-3
+              platform-tools
+              platforms-android-35
+              platforms-android-34
+              platforms-android-33
+              platforms-android-31
+              platforms-android-30
+              ndk-27-0-12077973
+              cmake-3-22-1
+            ]);
+          PWD = builtins.getEnv "PWD";
+          patchedFlutter = pkgs.flutter.override (prev: rec {
+            flutter = prev.flutter.overrideAttrs (prevAttrs: {
+              patches = prevAttrs.patches ++ [
+                # This patch is needed to avoid the Kotlin Gradle plugin writing to the store.
+                (pkgs.writeText "kotlin-fix.patch" ''
+                  --- a/packages/flutter_tools/gradle/build.gradle.kts
+                  +++ b/packages/flutter_tools/gradle/build.gradle.kts
+                  @@ -4,6 +4,8 @@
+
+                   import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+                  +gradle.startParameter.projectCacheDir = layout.buildDirectory.dir("cache").get().asFile
+                  +
+                   plugins {
+                       `java-gradle-plugin`
+                       groovy
+                '')
+              ];
+              passthru = prevAttrs.passthru // {
+                sdk = flutter;
+              };
+            });
+          });
+        in
+          pkgs.mkShell {
+            CHROME_EXECUTABLE = lib.getExe pkgs.chromium;
+            ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+            ANDROID_NDK_ROOT = "${androidSdk}/libexec/android-sdk/ndk-bundle";
+            ANDROID_AVD_HOME = "${PWD}/.android/avd";
+            ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
+            FLUTTER_SDK = "${patchedFlutter}";
+            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/share/android-sdk/build-tools/34.0.0/aapt2";
+            LD_LIBRARY_PATH = "${PWD}/apps/onyx/build/linux/x64/debug/bundle/lib/:${PWD}/apps/onyx/build/linux/x64/release/bundle/lib/:${PWD}/apps/onyx/build/linux/x64/profile/bundle/lib/";
+            buildInputs = with pkgs; [
+              flutter_rust_bridge_codegen
+              chromium
+              patchedFlutter
+              melos
+              jdk17
+              androidSdk
+              at-spi2-core
+              clang
+              dart
+              dbus
+              util-linux
+              cmake
+              ninja
+              libsecret
+              android-tools
+              pkg-config
+              gtk3
+              glib
+              pcre2
+              pcre
+              libselinux
+              libsepol
+              libthai
+              libdatrie
+              xorg.libXdmcp
+              libxkbcommon
+              xorg.libXtst
+              libepoxy
+              libgcrypt
+              libgpg-error
+              apksigner
+              zenity
+              lerc
+              libsysprof-capture
+            ];
+          };
+        formatter = pkgs.alejandra;
+      };
+    };
+}
