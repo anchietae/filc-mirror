39b2cee0aab2da7c84602a82a850b85be3517550
Armand <4831c0@proton.me>
2025-07-17 13:28:29 +0200

    Update build_apk.sh to build using custom sdk for release builds

--- tools/linux/build_apk.sh
+++ tools/linux/build_apk.sh
@@ -1,8 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 cd firka
 
-flutter gen-l10n --template-arb-file app_hu.arb
-echo $1 | grep main >/dev/null \
-	&& flutter build apk --release --tree-shake-icons --split-per-abi --target-platform android-arm,android-arm64 \
-	|| flutter build apk --debug --target-platform android-arm,android-arm64,android-x64
+if [ -f "$HOME/.flutter_path" ]; then
+  flutter gen-l10n --template-arb-file app_hu.arb
+  echo $1 | grep main >/dev/null \
+    && flutter build apk --release --tree-shake-icons --split-per-abi --local-engine-src-path $(cat $HOME/.flutter_path)/engine/src --local-engine=android_release --local-engine-host=host_release --target-platform android-arm,android-arm64 \
+    || flutter build apk --debug --target-platform android-arm,android-arm64,android-x64
+else
+    flutter gen-l10n --template-arb-file app_hu.arb
+    echo $1 | grep main >/dev/null \
+        && flutter build apk --release --tree-shake-icons --split-per-abi --target-platform android-arm,android-arm64 \
+        || flutter build apk --debug --target-platform android-arm,android-arm64,android-x64
+fi
