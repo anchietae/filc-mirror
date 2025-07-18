fd9d06ce0ecbadab07c7c87391f1d568f27d3c8f
Armand <4831c0@proton.me>
2025-07-18 22:47:55 +0200

    use custom flutter engine for release builds

--- tools/linux/build_apk.sh
+++ tools/linux/build_apk.sh
@@ -2,14 +2,35 @@
 
 cd firka
 
-if [ -f "$HOME/.flutter_path" ]; then
-  flutter gen-l10n --template-arb-file app_hu.arb
-  echo $1 | grep main >/dev/null \
-    && flutter build apk --release --tree-shake-icons --split-per-abi --local-engine-src-path $(cat $HOME/.flutter_path)/engine/src --local-engine=android_release --local-engine-host=host_release --target-platform android-arm,android-arm64 \
-    || flutter build apk --debug --target-platform android-arm,android-arm64,android-x64
+flutter gen-l10n --template-arb-file app_hu.arb
+
+if [ "$1" = "main" ]; then
+  if [ -f "$HOME/.flutter_path" ]; then
+    sdk_path="$(cat $HOME/.flutter_path)"
+    echo "Using flutter sdk from: $sdk_path"
+
+    flutter build apk --release --tree-shake-icons \
+      --local-engine-src-path "$sdk_path/engine/src" \
+      --local-engine=android_release --local-engine-host=host_release \
+      --split-per-abi \
+      --target-platform android-arm
+    mv build/app/outputs/flutter-apk/app-armeabi-v7a-release.apk build/app/tmp/
+    flutter build apk --release --tree-shake-icons \
+      --local-engine-src-path "$sdk_path/engine/src" \
+      --local-engine=android_release_arm64 --local-engine-host=host_release \
+      --split-per-abi \
+      --target-platform android-arm64
+    mv build/app/outputs/flutter-apk/app-arm64-v8a-release.apk build/app/tmp/
+    flutter build apk --release --tree-shake-icons \
+      --local-engine-src-path "$sdk_path/engine/src" \
+      --local-engine=android_release_x64 --local-engine-host=host_release \
+      --split-per-abi \
+      --target-platform android-x64
+    mv build/app/tmp/* build/app/outputs/flutter-apk
+  else
+    echo "$HOME/.flutter_path not found!"
+    exit 1
+  fi
 else
-    flutter gen-l10n --template-arb-file app_hu.arb
-    echo $1 | grep main >/dev/null \
-        && flutter build apk --release --tree-shake-icons --split-per-abi --target-platform android-arm,android-arm64 \
-        || flutter build apk --debug --target-platform android-arm,android-arm64,android-x64
+  flutter build apk --debug --target-platform android-arm,android-arm64,android-x64
 fi
