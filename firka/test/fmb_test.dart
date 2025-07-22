e0c27307e4be6dad43d562b3d595f209d83aa5a0
anchietae <contact@anchietae.cc>
2025-07-22 12:43:15 +0200

    replace fmb with FMBCrypt from git.anchietae.cc

--- firka/test/fmb_test.dart
+++ firka/test/fmb_test.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:firka/fmb_crypt.dart';
+import 'package:fmb_dart/fmb_dart.dart';
 
 void main() {
   test('encryption and decryption test', () async {
-    String? encrypted =
-        FMBCrypt.handleText('encrypt', 'Hello World!', 'test password');
+    String encrypted =
+        await FMBCrypt.handleText('encrypt', 'Hello World!', 'test password');
     expect(encrypted, isNotNull);
-    expect(encrypted, equals('eBzx7LhjOE7NW5Ba'));
     if (kDebugMode) {
       print('encrypted: $encrypted');
     }
-    String? decrypted =
-        FMBCrypt.handleText('decrypt', encrypted!, 'test password');
+    String decrypted =
+        await FMBCrypt.handleText('decrypt', encrypted, 'test password');
     expect(decrypted, isNotNull);
     expect(decrypted, equals('Hello World!'));
     if (kDebugMode) {
