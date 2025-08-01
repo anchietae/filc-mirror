aaf05f74a10c3b7b9117b5d6df7d3b645e62e400
anchietae <contact@anchietae.cc>
2025-08-01 16:03:30 +0200

    windows: flutter gen-l10n

--- tools/windows/build_apk.bat
+++ tools/windows/build_apk.bat
@@ -1,6 +1,9 @@
 @echo off
 
-cd ../../firka
+cd firka
 flutter clean
 flutter pub get
-flutter build apk --release --tree-shake-icons --split-per-abi --target-platform android-arm,android-arm64
+
+flutter gen-l10n --template-arb-file app_hu.arb
+
+flutter build apk --release --tree-shake-icons --split-per-abi --target-platform android-arm,android-arm64,android-x64
